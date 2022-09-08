ed70b30ec9a5049c46ac589e655c6da373cf7f43
Doleshor Khadka <doleshorkhad@gmail.com>
2022-09-07 22:04:00 -0400

    _commit-V1.50_

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_ui/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyUI());
+}
+
+class MyUI extends StatelessWidget {
+  const MyUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
